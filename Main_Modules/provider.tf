b3886f691dc02e0eb5befce0c1ab68e68b1090e5
Gyanesh Yadav <gyan9456@outlook.com>
2024-05-28 13:08:45 +0000

    Updated provider.tf

--- Main_Modules/provider.tf
+++ Main_Modules/provider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "azurerm" {
   features {}
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "testingRG"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storage67965"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "mycontainer45687"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
