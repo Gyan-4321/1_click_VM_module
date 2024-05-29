58d41c337bf840b6def1b108bbab892f60b5f32b
Gyanesh Yadav <gyan9456@outlook.com>
2024-05-29 11:58:59 +0000

    Updated data.tf

--- Modules_resources/nic/data.tf
+++ Modules_resources/nic/data.tf
@@ -3,3 +3,7 @@ data "azurerm_subnet" "testing" {
   virtual_network_name = "fortest-network"
   resource_group_name  = "raju-RG"
 }
+
+data "azurerm_public_ip" "testing" {
+  name                = azurerm_public_ip.testing.name
+  resource_group_name = azurerm_virtual_machine.fortest.resource_group_name
