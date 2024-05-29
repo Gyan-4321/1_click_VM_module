8ff486fbe9764b781e1e7d106d87252fa4c632c5
Gyanesh Yadav <gyan9456@outlook.com>
2024-05-29 12:59:29 +0000

    Updated data.tf

--- Modules_resources/VM/data.tf
+++ Modules_resources/VM/data.tf
@@ -10,3 +10,10 @@ data "azurerm_subnet" "fortest" {
   virtual_network_name = each.value.virtual_network_name
   resource_group_name  = each.value.resource_group_name
 }
+
+data "azurerm_network_security_group" "testing" {
+  for_each = nsgdata
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+}
+
