d4cc108178578b952b3a414b882ad926749afe16
Gyanesh Yadav <gyan9456@outlook.com>
2024-05-29 12:34:12 +0000

    Updated main.tf

--- Modules_resources/nic/main.tf
+++ Modules_resources/nic/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_network_interface" "fortest" {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.testing.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.testing.id
+    public_ip_address_id          = data.azurerm_public_ip.testing.id
     
   }
 }
